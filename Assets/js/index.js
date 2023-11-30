858f2fafdb1ecbb69726ff439c129702cec122e3
brxwnsugxr <jsdev.mas@gmail.com>
2023-11-30 23:06:00 +1100

    moving nav to right

---
+++ Assets/js/index.js
@@ -0,0 +1,11 @@
+function openNewPage(url) {
+  window.open(url, '_blank');
+}
+
+function hoverImage(element) {
+  element.style.opacity = '0.7';
+}
+
+function unhoverImage(element) {
+  element.style.opacity = '1';
+}
